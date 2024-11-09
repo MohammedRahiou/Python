daa2f228c24c6c70b7f50c7b9275627d2e334e95
MOHAMMED RAHIOUI <160219412+MohammedRahiou@users.noreply.github.com>
2024-11-09 16:09:46 +0100

    Add files via upload

---
+++ Tp exercice2.py
@@ -0,0 +1,13 @@
+import math
+print("Le périmètre d'un cercle de rayon")
+Rayon = float(input("Entre Rayon = "))
+
+Ppérimètre = 2*math.pi*Rayon
+print("Le périmètre d'un cercle de rayon est :",Ppérimètre)
+print("------------------------------------------------------------------------------")
+
+print("Le périmètre du rectangle ")
+a = float(input("Entre longeur = "))
+b = float(input("Entre largeur = "))
+Ppérimètre = 2*(a+b)
+print("Le périmètre du rectangle est :",Ppérimètre)
