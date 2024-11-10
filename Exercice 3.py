9fbea176e92e4a640da98a924bd7df84e06064a5
MOHAMMED RAHIOUI <160219412+MohammedRahiou@users.noreply.github.com>
2024-11-10 23:04:30 +0100

    Add files via upload

---
+++ Exercice 3.py
@@ -0,0 +1,6 @@
+import math
+a = float(input("Entre la valeur de a = "))
+b = float(input("Entre la valeur de b = "))
+c = float(input("Entre la valeur de c = "))
+Volume = (4/3)*math.pi*a*b*c
+print("La volume d'un ellipsoïde est :",Volume)
